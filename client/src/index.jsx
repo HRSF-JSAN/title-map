25f8c0dabda1d7040a17add0fa2bfad0f676f9e7
Michael Durfey <mdurfey@me.com>
2018-02-23 10:11:58 -0800

    tests initiated, circleCI added

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -43,4 +43,4 @@ class App extends React.Component {
   };
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
+// ReactDOM.render(<App />, document.getElementById('app'));
--- db/dbQuery.js
+++ db/dbQuery.js
@@ -11,7 +11,8 @@ const client = new Client({
 client.connect();
 
 
-  client.query('SELECT $1::text as message', ['Hello world!'], (err, res) => {
-    console.log(err ? err.stack : res.rows[0].message); // Hello World!
-    client.end();
-  });
+client.query('SELECT $1::text as message', ['Hello world!'], (err, res) => {
+  console.log(err ? err.stack : res.rows[0].message); // Hello World!
+  client.end();
+});
+
