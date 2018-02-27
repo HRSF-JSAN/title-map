6bedf09df19830c132bedfb7fe047974ceac3c4a
Michael Durfey <mdurfey@me.com>
2018-02-27 09:08:53 -0800

    fixed function name

--- __tests__/appSpec.jsx
+++ __tests__/appSpec.jsx
@@ -8,27 +8,27 @@ describe('data tests', () => {
   const app = new App();
 
   test('title and map data should exist', () => {
-    app.getDummyData(() => {
+    app.getRestaurant(() => {
       expect(app.state.titles).toHaveLength(2);
     });
   });
 
   test('map and title data should have a length of 200', () => {
-    app.getDummyData((err, title, maps) => {
+    app.getRestaurant((err, title, maps) => {
       expect(maps).toHaveLength(200);
       expect(title).toHaveLength(200);
     });
   });
 
   test('title data should have a title and type property', () => {
-    app.getDummyData((err, title) => {
+    app.getRestaurant((err, title) => {
       expect(title[0].title.exists()).toBe(true);
       expect(title[0].type.exists()).toBe(true);
     });
   });
 
   test('map data should have an address property', () => {
-    app.getDummyData((err, title, maps) => {
+    app.getRestaurant((err, title, maps) => {
       expect(maps[0].address.exists()).toBe(true);
     });
   });
