4eb4578483498ba7ee92645f15db34ed50c27ad2
Michael Durfey <mdurfey@me.com>
2018-03-06 08:07:22 -0800

    modified seed script and data

--- db/seedData.js
+++ db/seedData.js
@@ -1,20 +1,26 @@
 const client = require('./client');
 
-const titleData = require('./data/titleData.json');
+// const titleData = require('./data/titleData.json');
 const mapData = require('./data/mapData.json');
 const motherData = require('./data/MomaDummyData');
 
-Object.keys(motherData.types).forEach((type) => {
-  const queryString = 'insert into Types (type) values ($1)';
-  client.query(queryString, [type], (err) => {
-    if (err) throw new Error(err.stack);
-  });
+Object.keys(motherData.data).forEach((type, index) => {
+  const types = [];
+  if (types.indexOf(type) === -1) types.push(type);
+  if (index === motherData.data.length - 1) {
+    types.forEach((item) => {
+      const queryString = 'insert into Types (type) values ($1)';
+      client.query(queryString, [item], (err) => {
+        if (err) throw new Error(err.stack);
+      });
+    });
+  }
 });
 
 const insertData = (title, map) => {
   title.forEach((item) => {
     const queryString = 'INSERT INTO Restaurant (title, numstars, price, id) VALUES($1, $2, $3, $4)';
-    client.query(queryString, [item.title, item.numStars, item.price, item.id], (err) => {
+    client.query(queryString, [item.title, item.rating, item.price, item.id], (err) => {
       if (err) throw new Error(err.stack);
     });
     const associationTable = 'INSERT INTO Restaurant_Types (id_Restaurant, id_Types) VALUES($1, $2)';
@@ -36,10 +42,6 @@ const insertData = (title, map) => {
       });
   });
 };
-insertData(titleData, mapData, (err) => {
-  if (err) {
-    throw new Error(err);
-  }
-});
+insertData(motherData.data, mapData);
 
 exports.insertData = insertData;
