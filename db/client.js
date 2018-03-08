13e80717eabc3d94623f4c8d34e50a0cdfdc208f
Michael Durfey <mdurfey@me.com>
2018-03-07 20:49:29 -0800

    circle config

--- db/client.js
+++ db/client.js
@@ -1,9 +1,9 @@
-const { Client } = require('pg');
 require('dotenv').config();
+const { Client } = require('pg');
 
 const client = new Client({
-  host: process.env.POSTGRES_HOST || 'localhost',
-  port: process.env.POSTGRES_PORT || 'local socket',
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
   user: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PW,
   database: process.env.POSTGRES_DB,
