d9c45b29cd299c4d294b082b08ec89bbf49f2e6a
Michael Durfey <mdurfey@me.com>
2018-03-09 08:11:05 -0800

    deployment edits, added star asset

--- server/app.js
+++ server/app.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const path = require('path');
-const morgan = require('morgan'); // eslint-disable-line
 const routes = require('./router');
 const bodyParser = require('body-parser');
 
 const app = express();
-
-app.use(morgan('dev'));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
