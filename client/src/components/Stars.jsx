043586a183923da997cae6b46dcb510774b1097e
Michael Durfey <mdurfey@me.com>
2018-03-10 07:41:19 -0800

    deleted comment

--- client/src/components/Stars.jsx
+++ client/src/components/Stars.jsx
@@ -7,8 +7,6 @@ const uniqid = require('uniqid');
 const Stars = ({ rating }) => {
   const starNum = [];
   for (let i = 0; i < 5; i += 1) {
-    // es lint gave me an error here so I set no-unused-expressions to false in my eslintrc
-    // is there a more efficient or better way to add these star components?
     i < rating ? starNum.push(<Star className="redStar star" key={uniqid()} />) :
       starNum.push(<Star className="greyStar star" key={uniqid()} />);
   }
