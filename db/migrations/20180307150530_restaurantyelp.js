daf6644be70eca89a3ab2cb074a3e013f0fd8e20
Namson Leung <leungnamson@gmail.com>
2018-03-17 10:34:49 -0700

    updated knex schema with indexes

--- db/migrations/20180307150530_restaurantyelp.js
+++ db/migrations/20180307150530_restaurantyelp.js
@@ -3,7 +3,7 @@ const DOLLARSIGNS = ['$', '$$', '$$$', '$$$$', '$$$$$'];
 exports.up = (knex, Promise) => (
   Promise.all([
     knex.schema.createTable('restaurant', (table) => {
-      table.integer('id');
+      table.increments('id').primary();
       table.string('title');
       table.integer('rating');
       table.enu('price', DOLLARSIGNS);
@@ -13,15 +13,15 @@ exports.up = (knex, Promise) => (
       table.string('address');
       table.string('image');
       table.string('phonenumber');
-      table.integer('id_restaurant');
+      table.integer('id_restaurant').references('restaurant.id');
     }),
     knex.schema.createTable('types', (table) => {
-      table.increments('id');
+      table.increments('id').primary();
       table.string('type');
     }),
     knex.schema.createTable('restaurant_types', (table) => {
-      table.integer('id_types');
-      table.integer('id_restaurant');
+      table.integer('id_types').references('types.id');
+      table.integer('id_restaurant').references('restaurant.id')
     }),
   ])
 );
